6bfbb85b88f3b69a6197182743e51a5572723ea6
kumar804 <kumarsinghmanish547@gmail.com>
2021-08-06 07:34:55 +0530

    validating user first name with taste case

--- main/java/com/manish/UserRegistrationMain.java
+++ main/java/com/manish/UserRegistrationMain.java
@@ -1,7 +1,34 @@
 package com.manish;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistrationMain {
+    /*
+    creating function to validate first name using
+    regex pattern
+     */
+    public static boolean usernameValidation(String name){
+        String regex = "^[A-Z]{1}[a-z A-Z]{2,}$";
+        Pattern p = Pattern.compile(regex);
+        if (name == null)
+        {
+            return false;
+
+        }
+        Matcher m = p.matcher(name);
+        return m.matches();
+
+    }
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to junit Userregistration");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the your First Name : ");
+        String fname = input.nextLine();
+        System.out.println(usernameValidation(fname));
+
     }
 }
--- test/java/com/manish/UserRegistrationTest.java
+++ test/java/com/manish/UserRegistrationTest.java
@@ -1,4 +1,26 @@
 package com.manish;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 public class UserRegistrationTest {
+    UserRegistrationMain userRegistrationMain = new UserRegistrationMain();
+
+    /**
+     * Purpose : Validate First Name with first letter starting with capital and has minimum 3 characters in it.
+     * Return true if the string value passes else return false
+     */
+    @Test
+    void givenfirstnameshouldbeTrue() {
+        boolean result = userRegistrationMain.usernameValidation("Jon");
+        Assertions.assertEquals(result,true);
+    }
+
+    @Test
+    void givenfirstnameshouldbeFalse(){
+        UserRegistrationMain userRegistrationMain = new UserRegistrationMain();
+        boolean result = userRegistrationMain.usernameValidation("Jon@");
+        Assertions.assertEquals(result,false);
+    }
 }
